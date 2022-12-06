f23032abc77e210a5c39ceec4f240c079a76cc93
Aron Ingi Lenvig <aronlenvigg@gmail.com>
2022-12-06 22:57:54 +0000

    clean day 6 2022

--- 2022/day06_.py
+++ 2022/day06_.py
@@ -2,13 +2,13 @@ from aocd import get_data, submit
 data = get_data(day=6, year=2022).splitlines()
 line = data[0]
 
-def get_result(rep):
-    return next(i+rep for i in range(0, len(line))if len(set(line[i:i + rep])) == rep)
+def reaping_last_index(number):
+    return next(index+number for index in range(0, len(line)) if len(set(line[index:index + number])) == number)
 
 #part1
-result = get_result(4)
+result = reaping_last_index(4)
 submit(result, part="a", day=6, year=2022)
 
 #part2
-result = get_result(14)
+result = reaping_last_index(14)
 submit(result, part="b", day=6, year=2022)
