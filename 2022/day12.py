fea7d27cb7bd389ab6b703163bec6196d82cae25
Aron Ingi Lenvig <aronlenvigg@gmail.com>
2022-12-13 21:54:40 +0100

    day 12, 13

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,8 +1,60 @@
+from pprint import pprint
 from aocd import get_data, submit
-data = get_data(day=12, year=2022).splitlines()
+lines = get_data(day=12, year=2022).splitlines()
 
 #part1
-#submit(result, part="a", day=12, year=2022)
+from collections import deque
 
-#part2
-#submit(result, part="b", day=12, year=2022)
+graph = []
+for line in lines:
+    graph.append(line)
+row_lenght = len(graph)
+column_lenght = len(graph[0])
+
+# convert graph to numbers (1-26)
+graph_numbers = [[0 for _ in range(column_lenght)] for _ in range(row_lenght)]
+for r in range(row_lenght):
+    for c in range(column_lenght):
+        if graph[r][c]=='S':
+            graph_numbers[r][c] = 1
+        elif graph[r][c] == 'E':
+            graph_numbers[r][c] = 26
+        else:
+            graph_numbers[r][c] = ord(graph[r][c])-ord('a')+1
+
+
+def part1():
+    cordinations = deque()
+    for r in range(row_lenght):
+        for c in range(column_lenght):
+            if graph[r][c]=='S':
+                cordinations.append(((r,c), 0))
+    result = get_end_result(cordinations)
+    submit(result, part="a", day=12, year=2022)
+
+def part2():
+    cordinations = deque()
+    for r in range(row_lenght):
+        for c in range(column_lenght):
+            if graph_numbers[r][c] == 1: 
+                cordinations.append(((r,c), 0))
+    result = get_end_result(cordinations)
+    submit(result, part="b", day=12, year=2022)
+
+def get_end_result(cordinations : list):
+    tried_cordinations = set()
+    while cordinations:
+        (r,c),d = cordinations.popleft()
+        if (r,c) in tried_cordinations:
+            continue
+        tried_cordinations.add((r,c))
+        if graph[r][c]=='E':
+            return d
+        for r_direction,c_direction in [(-1,0),(0,1),(1,0),(0,-1)]:
+            r_new_direction = r + r_direction
+            c_new_direction = c + c_direction
+            if 0<=r_new_direction<row_lenght and 0<=c_new_direction<column_lenght and graph_numbers[r_new_direction][c_new_direction]<=1+graph_numbers[r][c]:
+                cordinations.append(((r_new_direction,c_new_direction),d+1))
+
+part1()
+part2()
--- 2022/day13.py
+++ 2022/day13.py
@@ -1,8 +1,70 @@
 from aocd import get_data, submit
-data = get_data(day=13, year=2022).splitlines()
+data = get_data(day=13, year=2022)
+from functools import cmp_to_key
+# seperate the data by double new line
+
+groups = data.split("\n\n")
 
 #part1
-#submit(result, part="a", day=13, year=2022)
+def part1():
+    result = 0
+    for i,group in enumerate(groups , start=1):
+        lines = group.splitlines()
+        list1 = eval(lines[0])
+        list2 = eval(lines[1])
+        value = compare(list1,list2)
+        if value == 1:
+            result += i
+    submit(result, part="a", day=13, year=2022)
 
-#part2
-#submit(result, part="b", day=13, year=2022)
+def part2():
+    items = []
+    for group in groups:
+        lines = group.splitlines()
+        items.append(eval(lines[0]))
+        items.append(eval(lines[1]))
+
+    items.append([[2]])
+    items.append([[6]])
+    
+    items = sorted(items, key=cmp_to_key(lambda item1,item2: compare(item1,item2)))[::-1]
+    result = 1
+    for i,p in enumerate(items,start=1):
+        if p==[[2]] or p==[[6]]:
+            result *= i
+    submit(result, part="b", day=13, year=2022)
+
+def compare(list1,list2):
+        
+    if type(list1) == int and type(list2) == int:
+        if list1 < list2:
+            return 1
+        if list1 == list2:
+            return 0
+        else:
+            return -1
+
+    if type(list1) == list and type(list2) == list:
+        i = 0
+        while i<len(list1) and i<len(list2):
+            value = compare(list1[i], list2[i])
+            if value == 1:
+                return 1
+            if value == -1:
+                return -1
+            i += 1
+        if i==len(list1) and i<len(list2):
+            return 1
+        elif i==len(list2) and i<len(list1):
+            return -1
+        else:
+            return 0
+
+    if type(list1) == list and type(list2) != list:
+        return compare(list1,[list2])
+
+    if type(list1) != list and type(list2) == list:
+        return compare([list1],list2)
+                
+# part2()
+# part1()
